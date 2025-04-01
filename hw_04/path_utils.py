593897021b142f2db930af6770b35a26a77060af
LeftCircle <leftcircle@DESKTOP-0F3NS95>
2025-04-01 13:53:26 -0400

    Spark spmv solution

---
+++ hw_04/path_utils.py
@@ -0,0 +1,294 @@
+"""
+Utility functions for path manipulation.
+"""
+
+import os
+
+
+def get_file_extension(file_path: str) -> str:
+	"""
+	Get the file extension from a file path.
+
+	Args:
+		file_path (str): The file path to extract the extension from.
+
+	Returns:
+		str: The file extension.
+	"""
+	return os.path.splitext(file_path)[1]
+
+
+def get_file_name_without_extension(file_path: str) -> str:
+	"""
+	Get the file name without the extension.
+	ex: C:/path/to/file.txt -> file
+
+	Args:
+		file_path (str): The file path to extract the name from.
+
+	Returns:
+		str: The file name without the extension.
+	"""
+	return os.path.splitext(os.path.basename(file_path))[0]
+
+
+def create_folder_if_not_exists(folder_path: str) -> None:
+	"""
+	Create a folder if it does not already exist.
+
+	Args:
+		folder_path (str): The folder path to create.
+	"""
+	if not os.path.exists(folder_path):
+		os.makedirs(folder_path)
+
+
+def get_file_number_suffix(file_path: str) -> int:
+	"""
+	Get the number suffix for a file.
+
+	Args:
+		file_path (str): The path to the file to get the number suffix from.
+
+	Returns:
+		int: The number suffix.
+	"""
+	name : str = os.path.basename(file_path)
+	extension : str = get_file_extension(name)
+	name_munus_extension : str = name.replace(extension, "")
+	number_str : str = name_munus_extension.split("_")[-1]
+	if number_str.isdigit():
+		return int(number_str)
+	return 0
+
+
+def remove_file_number_suffix(file_path: str) -> str:
+	"""
+	Remove the number suffix from a file name.
+
+	Args:
+		file_path (str): The path to the file to remove the number suffix from.
+
+	Returns:
+		str: The new file path with the number suffix removed.
+	"""
+	name : str = get_file_name_without_extension(file_path)
+	extension : str = get_file_extension(file_path)
+	number_str : str = name.split("_")[-1]
+	if number_str.isdigit():
+		tokens: list[str] = name.split("_")[:-1]
+		name = "_".join(tokens)
+	new_path : str = os.path.join(os.path.dirname(file_path), name + extension)
+	return new_path
+
+
+def _rename_until_new_number(file_path: str, count : int = 0) -> str:
+	"""
+	Rename a file until a new number is found.
+
+	Args:
+		file_path (str): The path to the file to rename.
+		count (int): The number suffix to start with.
+	
+	Returns:
+		str: The new path with the new name.
+	"""
+	dir : str = os.path.dirname(file_path)
+	name : str = get_file_name_without_extension(file_path)
+	extension : str = get_file_extension(file_path)
+	count += 1
+	new_name : str = f"{name}_{count:02}{extension}"
+	new_path : str = os.path.join(dir, new_name)
+	while os.path.exists(new_path):
+		assert(count < 100, "Too many files with the same name.")
+		new_name = f"{name}_{count:02}{extension}"
+		count += 1
+		new_path = os.path.join(dir, new_name)
+	return new_path
+
+
+def add_numbers_if_already_exists(file_path: str) -> str:
+	"""
+	Get the new name for a file based on the file path.
+	If a file with the same name exists, the new name will have 
+	a numeric suffix.
+
+	Args:
+		file_path (str): The path to the file to rename.
+
+	Returns:
+		str: the new path for the file.
+	"""
+	if not os.path.exists(file_path):
+		return file_path
+	file_number : int = get_file_number_suffix(file_path)
+	new_path : str = _rename_until_new_number(file_path, file_number)
+	return new_path
+
+
+def add_file_to_subdir(file_path: str, base_dir: str, subpath: str) -> None:
+	"""
+	Adds a file to a subdirectory.
+
+	Args:
+		file_path (str): The path to the file to move.
+		subpath (str): The subdirectory to move the file to.
+	"""
+	name : str = os.path.basename(file_path)
+	folder_dir : str = os.path.join(base_dir, subpath)
+	new_file_path : str = os.path.join(base_dir, subpath, name)
+	create_folder_if_not_exists(folder_dir)
+	new_file_path = add_numbers_if_already_exists(new_file_path)
+	os.rename(file_path, new_file_path)
+
+
+def get_file_name_for_subdir(file_path: str,
+							 base_dir: str, subpath: str) -> str:
+	"""
+	Get the new file name for a file in a subdirectory.
+
+	Args:
+		file_path (str): The path to the file to rename.
+		base_dir (str): The base directory for the file.
+		subpath (str): The subdirectory for the file.
+
+	Returns:
+		str: The new path for the file.
+	"""
+	name : str = os.path.basename(file_path)
+	new_file_path : str = os.path.join(base_dir, subpath, name)
+	new_file_path = add_numbers_if_already_exists(new_file_path)
+	return new_file_path
+
+
+def add_prefix_from_extension(file_path: str,
+							  extension_prefix: dict) -> str:
+	"""
+	Add a prefix to a file based on the extension.
+
+	Args:
+		file_path (str): The path to the file to rename.
+		extension_prefix (dict): The extension prefix dictionary.
+
+	Returns:
+		str: The new path for the file.
+	"""
+	extension : str = get_file_extension(file_path)
+	if extension in extension_prefix:
+		prefix : str = extension_prefix[extension]
+		new_name : str = f"{prefix}_{os.path.basename(file_path)}"
+		dir : str = os.path.dirname(file_path)
+		return os.path.join(dir, new_name)
+	return file_path
+
+
+def remove_strings_from_name(file_path: str, remove_str: list[str]) -> str:
+	"""
+	Remove strings from a file name that are not allowed.
+	ex: if 'small' is in the remove_str list, 
+	    C:/path/to_small/file_small_diffuse.png -> 
+	    C:/path/to_small/file_diffuse.png
+
+	Args:
+		file_path (str): The path to the file to rename.
+		remove_str (list): The list of strings to remove.
+
+	Returns:
+		str: The new path for the file.
+	"""
+	name : str = get_file_name_without_extension(file_path)
+	extension : str = get_file_extension(file_path)
+	name_parts : list[str] = name.split("_")
+	name_parts = [
+		part for part in name_parts if part.lower() not in remove_str
+	]
+	name = "_".join(name_parts)
+	return os.path.join(os.path.dirname(file_path), f"{name}{extension}")
+
+
+def replace_strings_in_name(file_path: str, replace_str: dict) -> str:
+	"""
+	Replace strings in a file name.
+	ex: if {'diffuse' : "C"} is in the replace_str dictionary,
+		C:/path/to_diffuse/file_diffuse.png -> C:/path/to_diffuse/file_C.png
+
+	Args:
+		file_path (str): The path to the file to rename.
+		replace_str (dict): The dictionary of strings to replace.
+
+	Returns:
+		str: The new path for the file.
+	"""
+	name : str = get_file_name_without_extension(file_path)
+	extension : str = get_file_extension(file_path)
+	name_parts : list[str] = name.split("_")
+	for i, part in enumerate(name_parts):
+		if part.lower() in replace_str:
+			name_parts[i] = replace_str[part.lower()]
+	name = "_".join(name_parts)
+	dir : str = os.path.dirname(file_path)
+	return os.path.join(dir, f"{name}{extension}")
+
+
+def clean_underscores_from_name(file_path: str) -> str:
+	"""
+	Clean up underscores from a file name.
+	ex: C:/path/to__file__diffuse.png -> C:/path/to/file_diffuse.png
+
+	Args:
+		file_path (str): The path to the file to rename.
+
+	Returns:
+		str: The new path for the file.
+	"""
+	name : str = get_file_name_without_extension(file_path)
+	extension : str = get_file_extension(file_path)
+	while "__" in name:
+		name = name.replace("__", "_")
+	if name.startswith("_"):
+		name = name[1:]
+	if name.endswith("_"):
+		name = name[:-1]
+	dir : str = os.path.dirname(file_path)
+	return os.path.join(dir, f"{name}{extension}")
+
+def get_all_files_of_suffix(folder_path: str, suffix: str) -> list[str]:
+	"""
+	Get all files in a folder with a specific suffix.
+
+	Args:
+		folder_path (str): The path to the folder to search.
+		suffix (str): The suffix to search for.
+
+	Returns:
+		list: The list of files with the specified suffix.
+	"""
+	files : list[str] = []
+	for root, _, file_names in os.walk(folder_path):
+		for file_name in file_names:
+			if file_name.endswith(suffix):
+				files.append(os.path.join(root, file_name))
+	return files
+
+def get_current_folder() -> str:
+	"""
+	Get the current folder.
+
+	Returns:
+		str: The current folder.
+	"""
+	return os.path.dirname(os.path.abspath(__file__))
+
+def get_full_path_from_relative_path(relative_path: str) -> str:
+	"""
+	Get the full path from a relative path.
+
+	Args:
+		relative_path (str): The relative path to convert.
+
+	Returns:
+		str: The full path.
+	"""
+	cur_folder : str = get_current_folder()
+	full_path : str = os.path.join(cur_folder, relative_path)
+	return os.path.abspath(full_path)
---
+++ hw_04/spmv-spark.py
@@ -0,0 +1,158 @@
+from pyspark.sql import SparkSession
+from pyspark.rdd import RDD
+import numpy as np
+from scipy.io import mmread
+from scipy import sparse
+from typing import List, Tuple, Any
+import time
+import path_utils
+import argparse
+
+def read_coo_matrix(filename: str) -> sparse.coo_matrix:
+	matrix = mmread(filename)
+	return matrix.tocoo()
+
+def sequential_spmv(coo_matrix: sparse.coo_matrix,
+					x_vector: np.ndarray) -> np.ndarray:
+	"""Sequential Sparse Matrix-Vector Multiplication (SpMV)."""
+	time_start = time.time()
+	result = np.zeros(coo_matrix.shape[0])
+	for i, j, v in zip(coo_matrix.row, coo_matrix.col, coo_matrix.data):
+		result[i] += v * x_vector[j]
+	time_end = time.time()
+	print(f"Sequential SpMV time: {time_end - time_start:.4f} seconds")
+	return result
+
+def multiply_row(row_data: Tuple[int, List[Tuple[int, float]]],
+                 x_broadcast: Any) -> Tuple[int, float]:
+	"""Multiply row elements with corresponding x values"""
+	row_id, col_val_pairs = row_data
+	result: float = 0.0
+	for col, val in col_val_pairs:
+		result += val * x_broadcast.value[col]
+	return (row_id, result)
+
+def spmv_coo_spark(spark: SparkSession,
+                   coo_matrix: sparse.coo_matrix,
+                   x_vector: np.ndarray) -> np.ndarray:
+	"""Sparse matrix-vector multiplication using COO format in Spark."""
+	
+	# Convert COO matrix to RDD of (row, (col, val)) pairs
+	matrix_rdd: RDD = spark.sparkContext.parallelize(
+		zip(coo_matrix.row, coo_matrix.col, coo_matrix.data)
+	)
+	matrix_rdd = matrix_rdd.map(lambda x: (x[0], (x[1], x[2])))
+	
+	# Broadcast the vector x
+	x_broadcast = spark.sparkContext.broadcast(x_vector)
+	
+	time_start = time.time()
+	# Perform the multiplication
+	result_rdd: List[Tuple[int, float]] = matrix_rdd.groupByKey()\
+						   				 .map(lambda x: multiply_row(x, x_broadcast))\
+						   				 .collect()
+	time_end = time.time()
+	print(f"Spark SpMV time: {time_end - time_start:.4f} seconds")
+	
+	#unpack_time = time.time()
+	# Collect results into a dense array
+	result: np.ndarray = np.zeros(coo_matrix.shape[0])
+	for row_id, value in result_rdd:
+		result[row_id] = value
+	
+	#unpack_time = time.time() - unpack_time
+	#print(f"Unpacking time: {unpack_time * 1000:.4f} ms")
+	return result
+
+def benchmark_spmv_spark(spark: SparkSession,
+                		 coo_matrix: sparse.coo_matrix,
+                		 x_vector: np.ndarray,
+						 num_iterations=100) -> float:
+	"""Benchmark SpMV using Spark with COO format."""
+
+	# Convert COO matrix to RDD of (row, (col, val)) pairs
+	matrix_rdd: RDD = spark.sparkContext.parallelize(
+		zip(coo_matrix.row, coo_matrix.col, coo_matrix.data)
+	)
+	matrix_rdd = matrix_rdd.map(lambda x: (x[0], (x[1], x[2])))
+	
+	# Broadcast the vector x
+	x_broadcast = spark.sparkContext.broadcast(x_vector)
+	
+	# Warmup
+	start_time = time.time()
+	result_rdd: List[Tuple[int, float]] = matrix_rdd.groupByKey()\
+						   				 .map(lambda x: multiply_row(x, x_broadcast))\
+						   				 .collect()
+	warmup_time = time.time() - start_time
+	print(f"\tWarmup time: {warmup_time * 1000:.4f} ms")
+
+	# Benchmark
+	times = []
+	for _ in range(num_iterations):
+		start_time = time.time()
+		result_rdd: List[Tuple[int, float]] = matrix_rdd.groupByKey()\
+						   				 .map(lambda x: multiply_row(x, x_broadcast))\
+						   				 .collect()
+		times.append(time.time() - start_time)
+	
+	avg_time = np.mean(times) * 1000 
+	gflops = (2.0 * coo_matrix.nnz / (avg_time / 1000)) / 1e9
+	
+	print(f"\tPerforming {num_iterations} iterations")
+	print(f"\tbenchmarking Spark-SpMV: {avg_time:8.4f} ms ({gflops:5.2f} GFLOP/s)")
+	
+	return avg_time
+
+def test_spmv_spark():
+	"""Test the SpMV implementation using Spark."""
+	
+	spark = SparkSession.builder\
+			.appName("SpMV")\
+			.getOrCreate()
+	
+	# Read matrix
+	#file = path_utils.get_full_path_from_relative_path("test_matrices/D6-6.mtx")
+	file = path_utils.get_full_path_from_relative_path("test_matrices/bfly.mtx")
+	matrix = read_coo_matrix(file)
+	
+	# Create random vector x
+	x = np.random.random(matrix.shape[1])
+	
+	spark_result: np.ndarray = spmv_coo_spark(spark, matrix, x)
+	sequential_result: np.ndarray = sequential_spmv(matrix, x)
+	assert np.allclose(spark_result, sequential_result), "Results do not match!"
+	print("Results match!")
+	
+	spark.stop()
+
+def main():
+	parser = argparse.ArgumentParser(description='Sparse Matrix-Vector Multiplication using Spark')
+	parser.add_argument('matrix_path', type=str, help='Path to the matrix market file (.mtx)')
+	args = parser.parse_args()
+	# Read matrix
+	#matrix_dir: str = path_utils.get_full_path_from_relative_path("test_matrices")
+	#matrices: List[str] = path_utils.get_all_files_of_suffix(matrix_dir, ".mtx")
+	#for matrix_file in matrices:
+	matrix_file: str = path_utils.get_full_path_from_relative_path(args.matrix_path)
+	matrix_file_name = path_utils.get_file_name_without_extension(matrix_file)
+	print(f"Matrix: {matrix_file_name}")
+		# Read matrix
+		# Initialize Spark
+	spark = SparkSession.builder\
+			.appName("SpMV")\
+			.getOrCreate()
+	matrix = read_coo_matrix(matrix_file)
+	
+	# Create random vector x
+	x = np.random.random(matrix.shape[1])
+	
+	# Run benchmark
+	avg_time: float = benchmark_spmv_spark(spark, matrix, x, 3)
+	print(f"Average time: {avg_time:.4f} ms")
+
+	spark.stop()
+
+if __name__ == "__main__":
+	#main()
+	test_spmv_spark()
